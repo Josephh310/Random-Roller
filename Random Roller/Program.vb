64f3333c4efbf9118968ca44aedb5bccbb6cb978
Joseph Hill <josephh310@icloud.com>
2025-04-01 12:42:49 +0100

    Display available RNG options

--- Random Roller/Program.vb
+++ Random Roller/Program.vb
@@ -1,7 +1,23 @@
 Imports System
 
 Module Program
+    
+    Dim _modes As Object(,) = {{"Roll a dice", DiceRoll}, {"Flip a coin", Coinflip}}
+    
+    Private Function DiceRoll()
+        
+    End Function
+    
+    Private Function CoinFlip()
+        
+    End Function
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Display the options from the _mode array.
+        For i = 0 To _modes.GetUpperBound(0)
+            Console.Write($"{i} | ")
+            Console.WriteLine(_modes(i, 0))
+        Next
     End Sub
+    
 End Module
