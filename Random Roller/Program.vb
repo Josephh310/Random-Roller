957ff7c83f4ea0cf724029e9206d9e87ae47c376
Joseph Hill <josephh310@icloud.com>
2025-04-01 14:13:41 +0100

    Added selection sub. Progrm is now fully-functional.

--- Random Roller/Program.vb
+++ Random Roller/Program.vb
@@ -3,12 +3,22 @@ Imports System
 Module Program
     
     Dim ReadOnly Modes As Object() = {"Roll a dice", "Flip a coin"}
+    
+    Private Sub RunMode(selection As Integer)
+        'Use user selection to call intended subroutine.
+        Select Case selection
+            Case 1
+                DiceRoll()
+            Case 2
+                CoinFlip()
+        End Select
+    End Sub
 
     Private Sub CoinFlip()
         Dim randomSeed As New Random()
         Dim randomValue As Integer = randomSeed.Next(0, 6) + 1
         Console.WriteLine($"You rolled a {randomValue}.")
-    End Function
+    End Sub
     
     Private Sub DiceRoll()
         Dim randomFlip As Integer = New Random().Next(0, 2) + 1
@@ -17,36 +27,35 @@ Module Program
         Else
             Console.WriteLine("You rolled tails.")
         End If
-    End Function
+    End Sub
     
     Sub Main(args As String())
         'Display the options from the _mode array.
         For i = 0 To Modes.GetUpperBound(0)
             Console.WriteLine($"{i + 1} | {Modes(i)}")
         Next
-        'Take user input. Loop until sanity is checked.
-        Dim saneInput As Integer = -1
+        Console.WriteLine()
         Do
-            Console.WriteLine("Select an option: ")
-            Dim input As String = Console.ReadLine()
-            Dim inputParsed As Integer
-            'Attempt to convert input to Integer
-            If Integer.TryParse(input, inputParsed) Then
-                If inputParsed >= 1 And inputParsed <= Modes.GetUpperBound(0) + 1 Then
-                    saneInput = inputParsed
+            'Take user input. Loop until sanity is checked.
+            Dim saneInput As Integer = -1
+            Do
+                Console.Write("Select an option: ")
+                Dim input As String = Console.ReadLine()
+                Dim inputParsed As Integer
+                'Attempt to convert input to Integer
+                If Integer.TryParse(input, inputParsed) Then
+                    If inputParsed >= 1 And inputParsed <= Modes.GetUpperBound(0) + 1 Then
+                        saneInput = inputParsed
+                    End If
                 End If
-            End If
-            If saneInput = -1 Then
-                Console.WriteLine("Please enter a valid selection.")
-            End If
-        Loop Until saneInput <> -1
-        'User has entered a valid integer selection. Proceed.
-        Select Case saneInput
-            Case 1
-                DiceRoll()
-            Case 2
-                CoinFlip()
-        End Select
+                If saneInput = -1 Then
+                    Console.WriteLine("Please enter a valid selection.")
+                End If
+            Loop Until saneInput <> -1
+            'User has entered a valid integer selection. Proceed.
+            RunMode(saneInput)
+            Console.WriteLine()
+        Loop
     End Sub
     
 End Module
